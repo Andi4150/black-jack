cc69e8819960c85d8bb93de3ef93e0ce0a933558
Andi4150 <andrea.e.w@hotmail.co.uk>
2021-03-06 13:05:07 +0000

    the first files of our project

---
+++ main.py
@@ -0,0 +1,16 @@
+
+from BlackJackPcg.Deck import Deck
+
+
+class Player:
+    pass
+
+
+if __name__ == '__main__':
+    d = Deck()
+    d.init_deck()
+    d.print_deck()
+    print()
+    d.suffle_deck()
+    d.print_deck()
+    print()
